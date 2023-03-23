be401f0d7acfda8ef834a77d153202fa69b0d65e
Cory Baker <corybaker24@gmail.com>
2023-03-22 21:49:22 -0700

    started vehicle item

---
+++ react-app/src/components/Vehicle/vehicleItem.js
@@ -0,0 +1,21 @@
+import './vehicle.css'
+import { Link } from 'react-router-dom'
+
+const VehicleListItem = ({vehicle}) => {
+
+
+
+    return (
+
+<div className='vehicle-item-box'>
+
+    <div>pic</div>
+    <div>Name Year</div>
+    <div>rating price</div>
+
+
+</div>
+    )
+}
+
+export default VehicleListItem
