6ace53be0268121b8899e418abd6a41369e38c6e
Cory Baker <corybaker24@gmail.com>
2023-04-05 20:19:56 -0700

    added coded out code from booking-thunk git for vehicle item

--- react-app/src/components/Vehicle/vehicleItem.js
+++ react-app/src/components/Vehicle/vehicleItem.js
@@ -26,3 +26,42 @@ const VehicleListItem = (props) => {
 };
 
 export default VehicleListItem;
+
+
+// import "./vehicle.css";
+// import BookNow from "../Booking/bookNow";
+
+// const VehicleListItem = (props) => {
+//   //   const { vehicle, pickupDate, dropOffDate } = props;
+
+//   return (
+//     <div className="vehicle-item-box">
+//       <div>
+//         <img
+//           className="vehicle-item-image"
+//           alt=""
+//           src={props.vehicle.picture}
+//         ></img>
+//       </div>
+
+//       <div className="vehicle-info">
+//         <div className="vehicle-name">
+//           {props.vehicle.year} {props.vehicle.make} {props.vehicle.model}{" "}
+
+//         </div>
+//         <div>${props.vehicle.dailyPrice}/day</div>
+//         <BookNow
+//           pickupDate={props.pickupDate}
+//           dropOffDate={props.dropOffDate}
+//           vehicle={props.vehicle}
+//           address={props.address}
+//           city={props.city}
+//           state={props.state}
+//           country={props.country}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default VehicleListItem;
