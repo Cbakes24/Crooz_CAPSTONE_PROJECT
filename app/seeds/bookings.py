3552c99f832de5c1abb045f145b3bc7155f7481b
Cory Baker <corybaker24@gmail.com>
2023-04-05 19:04:39 -0700

    updated booking seeds with city address blah blah

--- app/seeds/bookings.py
+++ app/seeds/bookings.py
@@ -3,12 +3,13 @@ from datetime import datetime, timedelta
 
 
 def seed_bookings():
-    booking1 = Booking(pickup_date=datetime(2023,5,16,8,0,0), drop_off_date=datetime(2023,5,20,8,0,0), location='Miami', guest_id=5, vehicle_id=1)
-    booking2 = Booking(pickup_date=datetime(2023,6,20,8,0,0), drop_off_date=datetime(2023,6,27,8,0,0), location='New York', guest_id=6, vehicle_id=2)
-    booking3 = Booking(pickup_date=datetime(2023,7,20,8,0,0), drop_off_date=datetime(2023,7,27,8,0,0), location='San Francisco', guest_id=5, vehicle_id=3)
-    booking4 = Booking(pickup_date=datetime(2023,8,20,8,0,0), drop_off_date=datetime(2023,8,27,8,0,0), location='Miami', guest_id=1, vehicle_id=4)
-    booking5 = Booking(pickup_date=datetime(2023,9,20,8,0,0), drop_off_date=datetime(2023,9,27,8,0,0), location='New York', guest_id=2, vehicle_id=5)
-    booking6 = Booking(pickup_date=datetime(2023,10,20,8,0,0), drop_off_date=datetime(2023,10,27,8,0,0), location='Miami', guest_id=1, vehicle_id=4)
+    booking1 = Booking(pickup_date=datetime(2023,5,16,8,0,0), drop_off_date=datetime(2023,5,20,8,0,0), address='123 Lane', city='Miami', state='FL', country='USA', guest_id=5, vehicle_id=1)
+    booking2 = Booking(pickup_date=datetime(2023,6,20,8,0,0), drop_off_date=datetime(2023,6,27,8,0,0), address='123 Lane', city='New York', state='CA', country='USA', guest_id=6, vehicle_id=2)
+    booking3 = Booking(pickup_date=datetime(2023,7,20,8,0,0), drop_off_date=datetime(2023,7,27,8,0,0), address='123 Lane', city='San Francisco', state='CA', country='USA', guest_id=5, vehicle_id=3)
+    booking4 = Booking(pickup_date=datetime(2023,8,20,8,0,0), drop_off_date=datetime(2023,8,27,8,0,0), address='123 Lane', city='Miami', state='FL', country='USA', guest_id=1, vehicle_id=4)
+    booking5 = Booking(pickup_date=datetime(2023,9,20,8,0,0), drop_off_date=datetime(2023,9,27,8,0,0), address='123 Lane', city='New York', state='NY', country='USA', guest_id=2, vehicle_id=5)
+    booking6 = Booking(pickup_date=datetime(2023,10,20,8,0,0), drop_off_date=datetime(2023,10,27,8,0,0), address='123 Lane', city='Miami', state='FL', country='USA', guest_id=1, vehicle_id=4)
+
 
 # should i use guest_id=1 or guest_id=guest1.id where is the guest seeder made? or is that a user
     db.session.add(booking1)
