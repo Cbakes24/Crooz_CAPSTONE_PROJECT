87fba4b34ac36a7b69fe706e265d1f7bad0c280b
Cory Baker <corybaker24@gmail.com>
2023-04-12 12:04:13 -0700

    review now updating new ones and usernames are showing

--- react-app/src/components/Booking/bookingListItem.js
+++ react-app/src/components/Booking/bookingListItem.js
@@ -1,27 +1,34 @@
-
-import LeaveReviewButton from '../Review/leaveReviewButton';
-import './booking.css';
-const BookingListItem = ({booking}) => {
-
-
-
-
+import LeaveReviewButton from "../Review/leaveReviewButton";
+import { useSelector } from "react-redux";
+import "./booking.css";
+import { Link } from "react-router-dom";
+const BookingListItem = ({ booking }) => {
+  const currentUser = useSelector((state) => state.session.user);
 
   return (
     <div className="booking-item-box">
       <div>
-        <h3>{booking.vehicle.make} {booking.vehicle.model}</h3>
+        <h3>
+          {booking.vehicle.make} {booking.vehicle.model}
+        </h3>
+        <Link to={`/vehicles/${booking.vehicle.id}`}>View Car</Link>
         <img className="booking-item-image" src={booking.vehicle.picture}></img>
       </div>
 
       <div className="booking-info">
         <div className="booking-name">
-        {booking.location} {booking.pickupDate} {booking.dropOffDate} {booking.vehicleId}
+          {booking.location} {booking.pickupDate} {booking.dropOffDate}{" "}
+          {booking.vehicleId}
         </div>
         <div>{booking.guest.username}</div>
-    <div>
-      <LeaveReviewButton  bookingId={booking.id} vehicleId={booking.vehicleId}/>
-    </div>
+        {currentUser && currentUser.id === booking.guest.id ? (
+          <div>
+            <LeaveReviewButton
+              bookingId={booking.id}
+              vehicleId={booking.vehicleId}
+            />
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- react-app/src/components/Review/reviewListItem.js
+++ react-app/src/components/Review/reviewListItem.js
@@ -1,16 +1,28 @@
 import { useSelector, useDispatch } from "react-redux";
-
+import { useEffect} from "react";
+import { fetchBookings } from "../../store/booking";
 const ReviewListItem = ({review}) => {
+    const dispatch = useDispatch()
     const currentUser = useSelector((state) => state.session.user);
+    const bookingState = useSelector((state) => state.booking)
+    const booking = bookingState[review.bookingId]
+console.log(booking, 'REVIEW BOOKINGGGG')
+
+
+  useEffect(() => {
+    dispatch(fetchBookings());
+  }, [dispatch, ]);
   //figure out username need to get a state to get the user
-  //booking.guest.id === revier.userId filter then use that username
-    return (
+  /*booking.guest.id === revier.userId filter then use that username
+  */
+    return booking ? (
         <div>
+            {booking.guest.username}
            {review.rating}
           <p>{review.body}</p>
         </div>
 
-    )
+    ) : null
 }
 
 export default ReviewListItem
