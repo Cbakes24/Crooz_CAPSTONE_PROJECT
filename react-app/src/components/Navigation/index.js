547f0328bfba1a86803f9bc2828452505899ec81
Cory Baker <corybaker24@gmail.com>
2023-04-12 16:53:36 -0700

    added my sites

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,27 +8,45 @@ function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
-    <ul>
-      <li>
-        <NavLink className="navbar-box" exact to="/bookings/search">
-          Home
-        </NavLink>
-        <NavLink className="navbar-box" exact to="/vehicles/create">
-          Vehicle Form
-        </NavLink>
-        <NavLink className="navbar-box" exact to="/signup">
-          SignUp
-        </NavLink>
-        <NavLink className="navbar-box" exact to="/users/host">
-          My Page
-        </NavLink>
-      </li>
-      {isLoaded && (
+    <>
+      <header>
+        <div id="mylinks">
+          <a href="https://github.com/Cbakes24/Crooz_CAPSTONE_PROJECT">
+            <button>
+              GitHub <i class="fa-brands fa-github"></i>
+            </button>
+          </a>
+          <br></br>
+
+          <a href="https://www.linkedin.com/in/cory-baker-9738ba2a/">
+            <button>
+              LinkedIn <i class="fa-brands fa-linkedin"></i>
+            </button>
+          </a>
+        </div>
+      </header>
+      <ul>
         <li>
-          <ProfileButton user={sessionUser} />
+          <NavLink className="navbar-box" exact to="/bookings/search">
+            Home
+          </NavLink>
+          <NavLink className="navbar-box" exact to="/vehicles/create">
+            Vehicle Form
+          </NavLink>
+          <NavLink className="navbar-box" exact to="/signup">
+            SignUp
+          </NavLink>
+          <NavLink className="navbar-box" exact to="/users/host">
+            My Page
+          </NavLink>
         </li>
-      )}
-    </ul>
+        {isLoaded && (
+          <li>
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
+      </ul>
+    </>
   );
 }
 
